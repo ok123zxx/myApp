ce994d88d2ec1541bec8ec568abb9362472ab7a3
zengjian <zengjian@winhong.com>
2018-02-04 23:44:15 +0800

    修复bug

--- src/main/java/com/springboot/myapp/app/App.java
+++ src/main/java/com/springboot/myapp/app/App.java
@@ -3,11 +3,15 @@ package com.springboot.myapp.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.springboot.myapp.dao")
+@EntityScan("com.springboot.myapp.entity")
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "com.springboot.myapp")
+@ComponentScan(basePackages = {"com.springboot.myapp.service","com.springboot.myapp.web"})
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
